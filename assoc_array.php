375431647f0111c211579a925c0ca5fe5b663a50
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-08-21 23:48:16 +0530

    assoc_array

---
+++ assoc_array.php
@@ -0,0 +1,7 @@
+<?php
+$students=array('Amrendra'=>95,'Pratap'=>98,'Singh'=>100);
+print_r($students);echo "<br/>";
+echo $students['Amrendra'];echo "<br/>";
+echo $students['Singh']; echo "<br/>";
+echo $students['Pratap'];
+?>
