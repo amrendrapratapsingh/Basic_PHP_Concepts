e9a8b80169407bcaf42e06c2e222587d97f439a0
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-08-22 12:08:50 +0530

    Date_time_function_timezone

--- Date_time.php
+++ Date_time.php
@@ -1,17 +1,20 @@
 <?php
-echo time(); echo "<br/>";
-date_default_timezone_get('asia/kolkata');
+// coverting the date into time.. by using the function (strtotime)
+//echo time();  // it represent the time stamps as seconds
 
-echo date_default_timezone_get(); echo "<br/>"; //  we can get defaulttimezone which is availble in php.ini configuration file.;
+echo "<br/>";
+//date_default_timezone_set('Asia/kolkata');
+//$date=date_default_timezone_set('Asia/kolkata');
 
-echo date('D-M-Y H:i:s'); echo  "<br/>";
 
-//strtotime
+// converting the seconds into date.....
+$date='22-08-2018';
+echo $ds=strtotime($date);
+echo "<br/>";
+echo date('D-M-Y',$ds);
+echo "<br/>";
 
-$date= date('D-M-Y');
-$ds=strtotime($date);
-echo $ds;
-
+// converting the date into other format..
+echo date('Y-M-D');
 
 ?>
-
